b33198ce755326761906dafe2f413ca89282b898
Rahil Vahora <104057403+rahil1202@users.noreply.github.com>
2024-05-27 14:56:38 +0530

    Fix: Updated api routes

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,22 @@
-import express from "express";
-import orderRoutes from "./routes/orderRoutes";
-import { ApplicationError } from "./utils/error";
+import express from 'express';
+import orderRoutes from './routes/api/orderRoutes';
+import { ApplicationError } from './utils/error';
 
 const app = express();
 app.use(express.json());
 
-app.use("/api", orderRoutes);
+app.use('/api', orderRoutes);
 
 // Error handling middleware
-app.use(
-  (
-    err: Error,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction,
-  ) => {
-    if (err instanceof ApplicationError) {
-      return res.status(err.status).json({ error: err.message });
-    }
-    return res.status(500).json({ error: "Internal Server Error" });
-  },
-);
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  if (err instanceof ApplicationError) {
+    return res.status(err.status).json({ error: err.message });
+  }
+  return res.status(500).json({ error: 'Internal Server Error' });
+});
 
 const server = app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log('Server listening on port 3000');
 });
 
 export default server;
--- src/routes/orderRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createOrderController } from "../controller/orderController";
-
-const orderRoutes = Router();
-
-orderRoutes.post("/orders", createOrderController);
-
-export default orderRoutes;
